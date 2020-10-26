0c9e2127b2dbf99a95c53a1163d972c0d8e9d827
NatalieGorr <nag372@nyu.edu>
2020-10-26 18:19:26 +0000

    final edits for lab1

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # OSlab1
+
+Natalie Gorr
+October 26, 2020
+
+ This program creates a simple "Shell"
+ It has three built-in commands:printid, exit, and greet
+ It aslo has ls, pwd, ps, date, lscpu which are exectuted by forking
+ the current process and using execve()
+
+ printid - prints current process id number
+ exit - terminates the progtam
+ greet - prints "Hello"
+ ls - lists the files in the current directory
+ pwd - prints the path of the current working directory
+ ps - prints the status of the current processes
+ date - prints current date and time
+ lscpu - prints information about the current processes of the machine
--- nag372.c
+++ nag372.c
@@ -1,9 +1,20 @@
 /*
  * This program creates a simple "Shell"
+ * It has three built-in commands:printid, exit, and greet
+ * It aslo has ls, pwd, ps, date, lscpu which are exectuted by forking
+ * the current process and using execve()
+ *
+ * printid - prints current process id number
+ * exit - terminates the progtam
+ * greet - prints "Hello"
+ * ls - lists the files in the current directory
+ * pwd - prints the path of the current working directory
+ * ps - prints the status of the current processes
+ * date - prints current date and time
+ * lscpu - prints information about the current processes of the machine
  *
  * author: Natalie Gorr
- * date: May 6, 2020
- *
+ * date: October 27, 2020
  */
 
 
@@ -18,7 +29,7 @@
 #include <sys/wait.h>
 
 
-
+// declare functions printid() and greet()
 void printid();
 void greet();
 
@@ -27,58 +38,54 @@ int main(){
 
 
     while(1){
+
         printf("lab1> ");
 
+        //get user input for which command to execute in the current loop
         char input[50];
-
+        if(scanf("%s",input) == 0){
+           exit(0);
+        }
+        //initialize the first part of the "complete command string"
         char commandstart[50] = "/bin/";
 
-        //if(fgets(input,50,stdin) == NULL){
-        //    exit(0);
-        //}
+        printf("Parent Process %d\n", getpid());
 
-       if(scanf("%s",input) == 0){
-           exit(0);
-       }
-
-
-
-       printf("Parent Process %d\n", getpid());
-
-
-
+        //check if the user input is any of the three built-in commands,
+        // if so execute them
         if ( strcmp(input,"exit") == 0  ){
             exit(0);
-       }
+        }
 
-        if ( strcmp(input,"printid") == 0  ){
+        else if ( strcmp(input,"printid") == 0  ){
             printid();
-       }
+        }
 
-       if ( strcmp(input,"greet") == 0  ){
+        else if ( strcmp(input,"greet") == 0  ){
             greet();
-       }
+        }
 
-
+        //else command is is one which needs to use fork, or it is not a supported command
         else{
 
+            //concatenate the user input with the "command starter" string
             strcat(commandstart, input);
 
+            //use this so the parent waits for the child
             int child_status;
 
             pid_t pid = fork();
             if( pid == 0 ) {
                printf("Child process %d will execute the command %s\n", getpid(), input );
 
-               char * argv[]= {input, NULL};
-
-
+               // use execve to execue command
+                char * argv[]= {input, NULL};
                 execve(commandstart, argv, NULL);
-
 
                 printf("Command Not Found!\n");
                 exit(0);
             }
+            //end of waiting
             wait(&child_status);
         }
     }
